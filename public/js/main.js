401b585c9ec8a4496e0ede25e9bb2c6a39460a2b
Kevin Lam <klam@aei-wc-1301.aei.local>
2019-05-24 16:42:19 -0700

    Adds features for deleting post

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,15 @@
-main.js
+$(document).ready(() => {
+	$('.delete-article').on('click', (e) => {
+		$target = $(e.target);
+		const id = $target.attr('data-id');
+		$.ajax({
+			type: 'DELETE',
+			url: '/articles/'+id,
+			success: res => {
+				alert('Deleting Article');
+				window.location.href='/';
+			},
+			error: err => console.log(err)
+		});
+	});
+});
--- server/routes/articles.js
+++ server/routes/articles.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 let Article = require('../models/articles');
 
+// Relay the Add Article form
 router.get('/add', (req, res) => {
 	res.render('add_article', {
 		title: 'Add New',
@@ -10,6 +11,7 @@ router.get('/add', (req, res) => {
 	});
 });
 
+// Post the new user
 router.post('/add', (req, res) => {
 	const d = new Date();
 	const months = [
@@ -33,10 +35,42 @@ router.post('/add', (req, res) => {
 		} else {
 			console.log('arTiCle heLLa aDDed.');
 			res.redirect('/');
-		}
-	})
+		};
+	});
 });
 
+// Send the edited post to Mongo
+router.post('/edit/:id', (req, res) => {
+	const d = new Date();
+	const months = [
+		'January', 'February', 'March', 
+		'April', 'May', 'June','July', 
+		'August', 'September', 'October', 
+		'November', 'December'
+	];
+
+	let postDate = `${months[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
+
+	let article = {};
+	article.author = req.body.author;
+	article.title = req.body.title;
+	article.body = req.body.body;
+	article.category = req.body.category;
+	article.lastEdited = postDate;
+	let query = { _id: req.params.id };
+
+	Article.updateOne(query, article, err => {
+		if (err) {
+			console.log(err);
+			return;
+		} else {
+			console.log(`Article ${req.params.title} has been updated.`)
+			res.redirect('/');
+		};
+	});
+});
+
+// Display the selected article
 router.get('/:id', (req, res) => {
 	Article.findById(req.params.id, (err, article) => {
 		res.render('article', {
@@ -47,6 +81,39 @@ router.get('/:id', (req, res) => {
 	});
 });
 
+// Edit the selected article
+router.get('/edit/:id', (req, res) => {
+	Article.findById(req.params.id, (err, article) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('edit_article', {
+				title: 'Edit Article',
+				heading: 'Edit Article',
+				article: article
+			});
+		};
+	});
+});
+
+// Delete select post
+router.delete('/:id', (req, res) => {
+	let query = { _id: req.params.id };
+
+	Article.findById(req.params.id, (err, article) => {
+		if (err) {
+			console.log('Delete error', err);
+		} else {
+			Article.deleteOne(query, (error) => {
+				if (error) {
+					console.log('Delete error', error);
+				} else {
+					res.send('Success');
+				}
+			});
+		};
+	});
+});
 
 module.exports = router;
 
